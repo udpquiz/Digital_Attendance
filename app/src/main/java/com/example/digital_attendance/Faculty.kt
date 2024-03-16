8468b1ace8bac60ff27dd84ecb9d9e03a50359c4
Maaz <shaikhmaaz.1819@gmail.com>
2024-03-16 13:18:34 +0530

    faculty_class

---
+++ app/src/main/java/com/example/digital_attendance/Faculty.kt
@@ -0,0 +1,10 @@
+package com.example.digital_attendance.com.example.digital_attendance
+import java.io.Serializable
+
+class Faculty (
+    val code: String,
+    val name: String,
+    val email: String,
+    val password: String):
+    Serializable {
+}
