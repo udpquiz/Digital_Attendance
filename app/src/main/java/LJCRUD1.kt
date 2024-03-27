adf282936b6eda498b24eac8925dac220320a69c
ammar0611 <ammarmansuri1826@gmail.com>
2024-03-27 12:04:19 +0530

    student page hogaya 2

--- app/src/main/java/LJCRUD1.kt
+++ app/src/main/java/LJCRUD1.kt
@@ -199,6 +199,7 @@ class LJCRUD1(context: Context): SQLiteOpenHelper(context,"LJ_Crud",null,8) {
     fun student_schedule(date: String?,sem:String?,div:String?):Cursor?{
         val db = this.writableDatabase
         return db.rawQuery("Select * from $tab_schedule WHERE date='$date' AND sem='$sem' AND division='$div'",null)
+//    ammar
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
